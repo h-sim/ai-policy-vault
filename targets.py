1fbfc9beadc3d8d6b0018805a5aa0787ad7bab5b
h-sim <161280531+h-sim@users.noreply.github.com>
2026-01-26 16:32:08 +0900

    Update targets.py

--- targets.py
+++ targets.py
@@ -4,3 +4,4 @@ TARGETS = [
   {"impact": "Medium", "name": "OpenAI News (RSS)", "url": "https://openai.com/news/rss.xml"},
   {"impact": "Breaking", "name": "OpenAI OpenAPI Spec (YAML)", "url": "https://app.stainless.com/api/spec/documented/openai/openapi.documented.yml"},
 ]
+
